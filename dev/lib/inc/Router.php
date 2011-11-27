56d2fcc59579cb143b3ca074b0a605c97b3e92b3
Bloodman <Bloodman@gshost.eu>
2011-11-27 18:08:16 +0100

    Update

--- dev/lib/inc/Router.php
+++ dev/lib/inc/Router.php
@@ -9,7 +9,7 @@
      * @author     René Činčura (Bloodman Arun)
      * @copyright  Copyright (c) 2011 Bloodman Arun (http://www.yucat.net/)
      * @license    http://www.yucat.net/license GNU GPL License
-     * @version    Release: 0.2.8
+     * @version    Release: 0.2.9
      * @link       http://www.yucat.net/documentation
      * @since      Class available since Release 0.1.0
      */
@@ -26,6 +26,7 @@
             'class' => ''
             );
         private $route      = array();
+        
         
         
         public final function __construct() {
@@ -74,93 +75,16 @@
             if(count($this->route) > $i && method_exists($cDir . $this->address['class'], $this->route[$i])) {
                 $this->address['method'] = $this->route[$i];
             }
-           
-            
-            
-            
-            
-            echo $dir;
-            d($this->address);
-            d($this->route);
         }
        
+       
         
+        public final function getAddress() {
+            return $this->address;
+        }
         
-        /**
-         * With this function you can get $_GET and parse it
-         * @return array
-         */
-        public static function getAddress() {
-            $out = array();
-            
-            $i = 0;
-                
-            foreach($route as $val) {
-                $a = '/Presenter/' . implode('/', $out);
-                $dir = ROOT . $a . '/' . $val;
-            
-                if(is_dir($dir)) {
-                    $out['dir'] = $val;
-                } elseif(file_exists($dir . '.php')) {
-                    $out['class'] = $val;
-                } elseif(method_exists(str_replace('/', '\\', $a), $val)) {
-                    $out['method'] = $val;
-                } else {
-                    $out['key' . $i] = $val;
-                    $i++;
-                }
-            }
-            
-            if(empty($out['class'])) {
-                $out = array();
-                $out['class'] = 'Login';
-            }
-            
-            $out['dir'] = isset($out['dir']) ? $out['dir'] : NULL;
-            $out['class'] = isset($out['class']) ? $out['class'] : NULL;
-            $out['method'] = isset($out['method']) ? $out['method'] : NULL;
 
-            return $out;
-        }
-        
-        
-        public static function getLevel() {
-            $addr = self::getAddress();
-
-            if(isset($addr['class']) && isset($addr['method'])) {
-                return 3;
-            } elseif(isset($addr['dir']) && isset($addr['class'])) {
-                return 2;
-            } elseif(isset($addr['class'])) {
-                return 1;
-            } else {
-                return FALSE;
-            }
-        }
-        
-        
-        public static function getOnlyParam() {
-            $addr = self::getAddress();
-            
-            for($i=0;$i<self::getLevel();$i++) {
-                unset($addr[array_search(reset($addr), $addr)]);
-            }
-            return $addr;
-        }
-        
-        
-        public static function getOnlyAddress() {
-            $addr = self::getAddress();
-            $out = array();
-            
-            for($i=0;$i<self::getLevel();$i++) {
-                $out[array_search(reset($addr), $addr)] = $addr[array_search(reset($addr), $addr)];
-                unset($addr[array_search(reset($addr), $addr)]);
-            }
-            return $out;
-        }
-        
-        
+       
         /**
          * This function is for create a real URL
          * You can use e.g. 'User:Profile:ajaxGetForm password,auth_key' and his call class Profile
@@ -168,7 +92,7 @@
          * @param string $input
          * @return string
          */
-        public static function traceRoute($input) {
+        public final function traceRoute($input) {
             if(!is_array($input)) {
                 //Parse to call and arguments
                 $input = explode(' ', $input, 2);
@@ -194,14 +118,14 @@
          * Redirect to web by special syntax for traceRoute
          * @param string $input 
          */
-        public static function redirect($input, $inURL = FALSE) {
+        public final function redirect($input, $inURL = FALSE) {
             $search = self::traceRoute($input);
             
             if(!preg_match('@' . $search . '@', $_SERVER['SCRIPT_URI']) && $inURL || !$inURL) {
                 if(Ajax::isAjax()) {
-                    exit('{"redirect" : "' . self::traceRoute($input) . '"}');
+                    exit('{"redirect" : "' . $this->traceRoute($input) . '"}');
                 } else {
-                    header('location: ' . self::traceRoute($input));
+                    header('location: ' . $this->traceRoute($input));
                 }    
             }
         }
@@ -210,15 +134,16 @@
         /*
          * Vnutorny redirect, pouziva sa ked je zadana napr URL User/Statistic a chcete to presmerovat na User/Statistic/statistic
          */
-        public static function like($input) {
+        public final function like($input) {
             if(is_array($input)) { 
                 $input = implode(':', $input);
             }
+            
             $search = substr($input, 0, strrpos($input, ':'));
-            $search = self::traceRoute($search);
+            $search = $this->traceRoute($search);
             
             if(!preg_match('@' . $search . '/(.*)@', $_SERVER['SCRIPT_URI'])) {
-                self::redirect($input);
+                $this->redirect($input);
             }
         }
     }
