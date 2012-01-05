7b42d85e165f785cbbb799085e54dca45f5cfa1a
Bloodman <Bloodman@gshost.eu>
2012-01-05 19:28:55 +0100

    fixed String.php

--- dev/lib/inc/String.php
+++ dev/lib/inc/String.php
@@ -1,16 +1,15 @@
 <?php
     /**
-     * String is classs for working with string
+     * String is class for working with string
      *
      * @category   Yucat
-     * @package    Includes
-     * @name       string
+     * @package    Library
+     * @name       String
      * @author     Bloodman Arun
-     * @copyright  Copyright (c) 2011 Bloodman Arun (http://www.yucat.net/)
-     * @license    http://www.yucat.net/license GNU GPL License
-     * @version    Release: 0.1.0
+     * @copyright  Copyright (c) 2011 - 2012 by Yucat
+     * @license    http://www.yucat.net/license GNU GPLv3 License
+     * @version    Release: 0.1.1
      * @link       http://www.yucat.net/documentation
-     * @since      Class available since Release 0.1.0
      */
 
     namespace inc;
@@ -19,8 +18,9 @@
         
         /**
          * Replace a '?' in $args[0] by array of $args
-         * @param array $args
-         * @return string
+         * 
+         * @param array $args input array
+         * @return string replaced string
          */
         public static function paramsReplace(array $args) {
             $string = $args[0];
@@ -37,6 +37,12 @@
         }
         
         
+        /**
+         * Generate random key
+         * 
+         * @param integer $len Length of out key
+         * @return string key
+         */
         public static function keyGen($len) {
             $hash = '';
             $chars = '1234567890QWERTZUIOPLKJHGFDSAYXCVBNM';
